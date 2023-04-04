7f6d0b7d65173de2111a0bb9120727665d87a2c9
Your Name <you@example.com>
2023-04-04 21:37:49 +0200

    selectors_update

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,15 +8,15 @@ from playwright.sync_api import expect
 def set_up(page):
     page.goto(login_page)
    
-    user_login = page.locator("[data-test=\"username\"]")
+    user_login = page.locator("[data-test='username']")
     user_login.click()
     user_login.fill("standard_user")
  
-    user_pass = page.locator("[data-test=\"password\"]")
+    user_pass = page.locator("[data-test='password']")
     user_pass.click()
     user_pass.fill("secret_sauce")
     
-    login_button = page.locator("[data-test=\"login-button\"]")
+    login_button = page.locator("[data-test='login-button']")
     login_button.click()      
     expect(page).to_have_url(home_page)
 
